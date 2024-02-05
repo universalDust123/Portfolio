5244c8f21dbe676a70b816dbc7d0e9135a2b4eda
Satyam Kumar <satyam73233@gmail.com>
2024-02-05 17:11:22 +0530

    Changes in form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,33 +1,55 @@
 import "./FormStyle.css"
-import React from 'react'
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { useRef } from "react"
+import {BsPatchCheckFill} from 'react-icons/bs'
 
-class Form  extends React.Component{
-//   const data = useRef(0);
-//   const nameInput = useRef(null)
 
-//   console.log(name);
+const Form = ()=> {
+  const form = useRef();
+  const [emailSend,setEmailSend]=useState(false);
+  
 
-handleSubmit = (event) =>{
+// Send email using EmailJs by useRef()
+const handleSubmit = (event) =>{
     event.preventDefault();
-    console.log(event.target.name.value);
-    console.log(event.target.email.value);
+
+    emailjs
+    .sendForm('service_ezyy0bo', 'template_nchkr5i', form.current, {
+        publicKey: 'bMAmRZqqXftA2DTa1',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        //   prompt("Your Message has been sent")
+        setEmailSend(true);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+
 }
-render(){
+
 
     return (
         <div className="form-content">
-            <form onSubmit={this.handleSubmit}>
+            <form ref={form} onSubmit={handleSubmit}>
                 <label>Name</label>
                 <input type="text" name="name"></input>
                 <label>Email</label>
-                <input type="email" name="email"></input>
+                <input type="email" name="email" ></input>
                 <label>Message</label>
-                <textarea rows={5} placeholder="Type Your message.."/>
-                <button type="submit" className="btn btn-submit">Submit</button>
+                <textarea rows={5} placeholder="Type Your message.." name="message"/>
+                <button type="submit" value="Send" className="btn btn-submit">Submit</button>
             </form>
+            {emailSend&&<div className="sendEmail">
+            <BsPatchCheckFill color="#3a5a40" size="3rem"/>
+                <h2>Email Sent Successfully</h2>
+            </div>}
         </div>
     )
-}
+
 }
 
 export default Form
